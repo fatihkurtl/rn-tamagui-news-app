106a42200bbbc7c1fa4c82d48549b5b869ecbae3
fatihkurtl <kurtf061@gmail.com>
2024-09-20 21:40:30 +0300

    project folders deleted

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,37 +1,24 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { useFonts } from 'expo-font';
+import { TamaguiProvider, YStack } from 'tamagui';
+import config from '../../tamagui.config';
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+export default function RootLayout() {
+  const [loaded] = useFonts({
+    Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
+    InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
+  });
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
+  if (!loaded) {
+    return null;
+  }
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
-  );
+    // add this
+    <TamaguiProvider config={config}>
+      <YStack>
+      
+      </YStack>
+    </TamaguiProvider>
+  )
 }
+
